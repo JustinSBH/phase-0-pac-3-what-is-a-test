4bcec5d2bf736aeb7239bf73db6f0fb9b08d7e51
Justin Repnior Labastere Usprevil <jrepnior@gmail.com>
2025-04-03 14:00:58 -0400

    Test

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const name = "Susan";
-const height = 14;
-const message = `${name} is ${height} inches tall`;
-module.exports = {name, height, message};
-console.log(message);
+function seulesayHiToHeadphonedRoommate(string) {
+    if(string.toUpperCase()){
+        console.log('OUI, EN EFFET !');
+    }
+
+    if(string.toLowerCase()){
+        console.log('Je ne vous entends pas !');
+    }
+
+    if(string = "Dînons ensemble !"){
+        console.log("J'adorerais !");
+    }
+
+    return string.toUpperCase();
+}
+
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+  
